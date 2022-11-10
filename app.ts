fd986894b20e1272fd566213a0398deb504645bd
ASelviyan <angelikaselviyan5@gmail.com>
2022-11-09 17:25:51 -0700

    future value types

--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
 // A function that adds two numbers 
-// adding these types next the arguments makes sure that the argement is a spacific type
-// we do that so that the program doesnt get confused during develpment
+// adding these types next the arguments makes sure that the argument is a specific type
+// we do that so that the program doesn't get confused during development
 function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     if(showResult) {
         console.log(phrase + (n1 + n2))
     }
     return n1 + n2
 }
+//You can assigned a variable a type(number) so that typescript will know what value will eventually be there
+let futureNumber: number;
+futureNumber = 5;
+
+
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
